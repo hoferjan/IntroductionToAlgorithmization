058d9b1fade1b49d723c0f493fd5679158b79692
Hans <jhoferj@protonmail.com>
2022-12-05 17:55:02 +0100

    lucky tickets finished

--- Cviko11/luckytickets.py
+++ Cviko11/luckytickets.py
@@ -6,17 +6,18 @@ class LuckyNumberCounter:
 
   def __init__(self, digitCount):
     self.digitCount = digitCount
-    self.numberCount = 1
+    self.numberCount = (10 **digitCount) - 1
 
   # as many methods as you need...
 
   def isLuckyNumber(self, number):
-      first_half = number // (10)**((len(str(number)))/2)
-      second_half = number - (first_half * 10 **((len(str(number)))/2))
       first_half_sum = 0
       second_half_sum = 0
-      first_half = list(str(first_half))
-      second_half = list(str(second_half))
+      first_half = number.copy()
+      second_half = number.copy()
+      for i in range(len(number)//2):
+          first_half.pop()
+          second_half.pop(0)
       for num in first_half:
           first_half_sum += int(num)
       for num in second_half:
@@ -25,38 +26,27 @@ class LuckyNumberCounter:
           return True
       else:
           return False
-
   def getLuckyNumberCount(self):
-        if self.digitcount // 2 == 0:
-            return None
-        else:
-            pass # tady dopis funkci
+    sum = 0
+    if self.digitCount % 2 != 0:
+        return None
+    for number in range(self.numberCount+1):
+        number = list(str(number))
+        while len(number) != self.digitCount:
+            number.insert(0,"0")
+        if self.isLuckyNumber(number):
+            sum +=1
+        print(number)
+    return sum
 
 
-# digits = 6
-# lnc = LuckyNumberCounter(digits)
-# luckyNumbersTotal = lnc.getLuckyNumberCount()
-# luckyNumbersProbab = luckyNumbersTotal / lnc.numberCount
-#
-# print(lnc.digitCount, "digit lucky numbers count:", luckyNumbersTotal)
-# print("Probability of getting a lucky number is:", luckyNumbersProbab, end=" ")
-# print("so chance is 1 in", 1 / luckyNumbersProbab)
+digits = 6
+lnc = LuckyNumberCounter(digits)
+luckyNumbersTotal = lnc.getLuckyNumberCount()
+luckyNumbersProbab = luckyNumbersTotal / lnc.numberCount
+
+print(lnc.digitCount, "digit lucky numbers count:", luckyNumbersTotal)
+print("Probability of getting a lucky number is:", luckyNumbersProbab, end=" ")
+print("so chance is 1 in", 1 / luckyNumbersProbab)
 
 
-def isLuckyNumber(number):
-    first_half = number // (10)**((len(str(number)))/2)
-    second_half = number - (first_half * 10 ** ((len(str(number)))/2))
-    first_half_sum = 0
-    second_half_sum = 0
-    first_half = list(str(int(first_half)))
-    second_half = list(str(int(second_half)))
-    for num in first_half:
-        first_half_sum += int(num)
-    for num in second_half:
-        second_half_sum +=int(num)
-    if second_half_sum == first_half_sum:
-        return True
-    else:
-        return False
-
-print(isLuckyNumber(111111111))
--- Cviko11/test.py
+++ Cviko11/test.py
@@ -1,3 +1,7 @@
-lst = [1,2,3,4]
-lst.reverse()
-print(lst[1])
+lst = [1, 2, 3, 4]
+lst.insert(0, "x")
+print(lst)
+lst1 = lst[0:len(lst)]
+print(lst1)
+lst1.pop(0)
+print(lst1)
